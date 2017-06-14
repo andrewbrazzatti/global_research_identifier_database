c5f03168406bafd63a286451bb1d36657a55a0be
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2017-06-14 15:19:31 +0930

    Added screen scraping to find the latest zip release of the GRID database from the download page

--- scraper.py
+++ scraper.py
@@ -1,11 +1,14 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
 from StringIO import StringIO
 from zipfile import ZipFile
 from urllib import urlopen
 import csv
 import scraperwiki
 import sys
+import requests
+import re
+from bs4 import BeautifulSoup
+
+
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
@@ -15,7 +18,23 @@ def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
     for row in csv_reader:
         yield [unicode(cell, 'utf-8') for cell in row]
 
-url = urlopen("https://ndownloader.figshare.com/files/8500607")
+# check the downloads page for the latest release (assumed to be the first download button we find)
+page = requests.get("https://grid.ac/downloads")
+soup = BeautifulSoup(page.content, 'html.parser')
+
+downloads = soup.find_all(text=re.compile('Download release'))
+link = "https:"+downloads[0].parent['href']
+
+# Visit the doi on figshare and get the download link
+page = requests.get(link)
+soup = BeautifulSoup(page.content, 'html.parser')
+downloads = soup.find_all('a', class_='download-button')
+download_link = downloads[0]['href']
+
+
+
+# Download the zip and load in the institutes CSV to the database
+url = urlopen(download_link)
 zipfile = ZipFile(StringIO(url.read()))
 
 
