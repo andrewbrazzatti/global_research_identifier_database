89a2ed95998600f9164d4a44b80f73271fb92831
Andrew Brazzatti <andrew@redboxresearchdata.com.au>
2017-06-14 12:44:44 +0930

    Initial commit of the scraper code

--- scraper.py
+++ scraper.py
@@ -1,24 +1,24 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
+from StringIO import StringIO
+from zipfile import ZipFile
+from urllib import urlopen
+import csv
+import scraperwiki
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+def unicode_csv_reader(utf8_data, dialect=csv.excel, **kwargs):
+    csv_reader = csv.reader(utf8_data, dialect=dialect, **kwargs)
+    for row in csv_reader:
+        yield [unicode(cell, 'utf-8') for cell in row]
+
+url = urlopen("https://ndownloader.figshare.com/files/8500607")
+zipfile = ZipFile(StringIO(url.read()))
+
+
+reader = unicode_csv_reader(zipfile.open("full_tables/institutes.csv"))
+for row in reader:
+  scraperwiki.sqlite.save(unique_keys=['grid_id'], data={"grid_id": row[0], "name": row[1], "wikipedia_url": row[2], "email_address": row[3], "established":row[4]})
